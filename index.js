1c4249e63e01b81f15071fde90219f5725453cb1
Amit Gueta <amit5333@gmail.com>
2021-12-25 19:29:35 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Bye! World!');
+})
+
+app.get('/amit', function (req, res) {
+    res.send('Hello, World!');
+  })
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+})
